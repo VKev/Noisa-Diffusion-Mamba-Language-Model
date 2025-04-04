Attention mask fix

inference.py
@@ -9,7 +9,7 @@ import yaml
 def get_args():
     parser = argparse.ArgumentParser(description="Inference with trained Noisa model")
     parser.add_argument("--config", type=str, default=r'config/noisa-tiny.yaml', help="Path to YAML config file")
-    parser.add_argument("--model_path", type=str, default=r'./checkpoints/noisa-epoch=1341-train_loss=0.00.ckpt', help="Path to the trained model checkpoint")
+    parser.add_argument("--model_path", type=str, default=r'./checkpoints/noisa-epoch=250-train_loss=0.08.ckpt', help="Path to the trained model checkpoint")
     parser.add_argument("--max_length", type=int, default=1024, help="Maximum tokenization length")
     parser.add_argument("--embed_dim", type=int, default=256, help="Embedding dimension")
     parser.add_argument("--num_heads", type=int, default=4, help="Number of attention heads")
@@ -48,6 +48,7 @@ def stream_inference(model, tokenizer, prompt, max_length, temperature=0.1):
         for _ in range(max_length):
             seq_len = input_ids.size(1)
             attention_mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool().to(input_ids.device)
+            attention_mask = attention_mask.unsqueeze(0).expand(input_ids.size(0), -1, -1)
             logits = model(input_ids, attention_mask=attention_mask)
             next_token_logits = logits[:, -1, :] / temperature
             next_token = torch.argmax(next_token_logits, dim=-1, keepdim=True)

model/attention.py
@@ -10,8 +10,19 @@ class SelfAttention(nn.Module):
         self.norm = nn.LayerNorm(attention_module.d_model)
 
     def forward(self, x, attention_mask=None):
-        assert attention_mask is not None, "attention_mask must be provided"
-        attn_out = self.attn(x, x, x, attention_mask=attention_mask)
+        if attention_mask is not None:
+            batch_size, seq_len = x.shape[:2]
+            h = self.attn.h
+            attention_mask = attention_mask.bool()
+            if attention_mask.dim() == 2:
+                attention_mask = attention_mask.unsqueeze(1).unsqueeze(1)
+                attention_mask = attention_mask.expand(-1, h, seq_len, -1)
+            elif attention_mask.dim() == 3:
+                attention_mask = attention_mask.unsqueeze(1).expand(-1, h, -1, -1)
+
+            attn_out = self.attn(x, x, x, attention_mask=attention_mask)
+        else:
+            attn_out = self.attn(x, x, x)
         x = x + attn_out
         x = self.norm(x)
         return x

train.py
@@ -37,14 +37,14 @@ class TrainingModule(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters(ignore=['model'])
         self.model = model
-        self.model.register_buffer("attention_mask", torch.triu(torch.ones(max_length, max_length), diagonal=1).bool())
 
     def forward(self, input_ids, attention_mask):
         return self.model(input_ids, attention_mask)
 
     def training_step(self, batch, batch_idx):
         input_ids = batch['input_ids']
-        attention_mask = self.model.attention_mask
+        attention_mask = batch['attention_mask']
+        
         logits = self(input_ids, attention_mask)
         logits = logits[:, :-1, :]
         targets = input_ids[:, 1:]